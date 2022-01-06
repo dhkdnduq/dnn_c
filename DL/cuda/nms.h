e4716133071c44dbaf6f1e6963133636589f070f
ojun.kwon <dhkdnduq@gmail.com>
2022-01-06 09:22:54 +0900

    Create nms.h

---
+++ DL/cuda/nms.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <driver_types.h>
+namespace dnn {
+namespace cuda {
+
+int nms(int batchSize, const void *const *inputs, void *const *outputs,
+        size_t count, int detections_per_im, float nms_thresh, void *workspace,
+        size_t workspace_size, cudaStream_t stream);
+
+}
+}  // namespace odtk
