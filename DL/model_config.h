5c99fa43f9b5faf09859d7a68a9596fd707998a4
ojun.kwon <dhkdnduq@gmail.com>
2022-01-07 09:04:23 +0900

    Update model_config.h

--- DL/model_config.h
+++ DL/model_config.h
@@ -5,6 +5,9 @@ class  model_config {
 public:
 	model_config();
 	bool load_config(string configpath);
+	vector<double> tokenize_d(const string& data, const char delimiter);
+  vector<string> tokenize_s(const string& data, const char delimiter);
+
 public:
 
 	int dnn_width;
@@ -17,19 +20,15 @@ public:
 	int dlaCore{ -1 };                   //!< Specify the DLA core to run network on.
 	bool int8{ false };                  //!< Allow runnning the network in Int8 mode.
 	bool fp16{ false };                  //!< Allow running the network in FP16 mode.
-  bool mean_sub_enable {true};
-  bool is_nchw;//(tensorflow model == false) (pytorch model == true) 
-	std::vector<std::string> dataDirs; //!< Directory paths where sample data files are stored
+  bool is_use_mean_sub {true};
+  bool is_nchw;
 	string modelFileName;
-	bool anomalyEnable = false;;
+  bool anomalyEnable = false;;
   vector<anomaly_var> vanomaly;
-  std::vector<double> mean = {0.5, 0.5, 0.5};
-  std::vector<double> std = {0.5, 0.5 ,0.5};
 
-	vector<double> tokenize_d(const string& data, const char delimiter);
-  vector<string> tokenize_s(const string& data, const char delimiter);
+  vector<double> mean{0.485, 0.456, 0.406};
+  vector<double> std{0.229, 0.224, 0.225};
 	//for yolact
-
   int yolact_max_size;
   int yolact_min_size ;
 
