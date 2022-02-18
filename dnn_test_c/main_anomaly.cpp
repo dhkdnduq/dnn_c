530209f232278fc6ef9bcc2b1301b6b847c9e6cb
ojun.kwon <dhkdnduq@gmail.com>
2022-02-18 13:42:30 +0900

    Update main_anomaly.cpp

--- dnn_test_c/main_anomaly.cpp
+++ dnn_test_c/main_anomaly.cpp
@@ -1,6 +1,3 @@
-
-//#include "TensorRT.h"
-//#include "Onnx.h"
 #include <chrono>
 #include <experimental/filesystem>
 #include <iostream>
@@ -10,30 +7,48 @@
 using namespace std;
 namespace fs = std::experimental::filesystem;
 
-int main_ano()
+int main()
 {
   fs::path cwd = fs::current_path();
   cout << "current dir : " << cwd.string() << endl;
   bool bload = torch_init("dnn_setting_anomaly_patchcore_bmw.json");
-  string dir_path = "D:\\deeplearning\\data\\mvtec_anomaly_detection\\bmw\\test\\ng\\";
+  string dir_path = "anomaly";
   string save_dir_path = "bmw\\";
-
-  for (auto& p : fs::directory_iterator(dir_path)) {
-    
+  int batch_size = 3;
+ 
+  for(int i=0;i<3;i++)
+  {
+    int idx = 0;
+    for (auto& p : fs::directory_iterator(dir_path)) {
       fs::path path = p;
       string filename = path.filename().string();
       string filepath = path.u8string();
-      segm_t_container_rst_list binary_container;
       torch_add_image_file(filepath.c_str());
-      torch_anomaly_detection(binary_container,0);
-      int batch_index = 0;
-      cout << binary_container[batch_index].candidates[0].prob << ","<< (float)binary_container[batch_index].candidates[0].obj_id<<endl;
-      //segm_t_container& segm = binary_container[batch_index];
-      //std::vector<unsigned char> dispdata(binary_container[batch_index].display_image.data, segm.display_image.data + segm.display_image.size);
-      //cv::Mat dispmat = cv::imdecode(dispdata, -1); 
-      //cv::imwrite(save_dir_path + filename, dispmat);
+      if (++idx >= 3) break;
+    }
+    segm_t_container_rst_list segm_container;
+    torch_anomaly_detection(segm_container);
+    
+    int batch_index = 0;
+    for (int batch_index = 0; batch_index < batch_size; batch_index++) {
+      // show result
+      segm_t_container& segm = segm_container[batch_index];
+      std::vector<unsigned char> dispdata(
+          segm.display_image.data,
+          segm.display_image.data + segm.display_image.size);
+      cv::Mat dispmat = cv::imdecode(dispdata, -1);
+      cv::imshow("detection", dispmat);
+      cv::waitKey(-1);
+    }
+    // cout << binary_container[batch_index].candidates[0].prob << ","<<
+    // (float)binary_container[batch_index].candidates[0].obj_id<<endl;
+    // cv::imdecode(dispdata, -1); cv::imwrite(save_dir_path + filename,
+    // dispmat);
 
   }
+  
+
+
   torch_dispose();
   getchar();
 	return 0;
