cba7c700de0f17ec7b12b13a08c4766752499666
ojun.kwon <dhkdnduq@gmail.com>
2022-02-18 13:39:42 +0900

    Update model_config.h

--- DL/model_config.h
+++ DL/model_config.h
@@ -5,9 +5,7 @@ class  model_config {
 public:
 	model_config();
 	bool load_config(string configpath);
-	vector<double> tokenize_d(const string& data, const char delimiter);
-  vector<string> tokenize_s(const string& data, const char delimiter);
-
+	
 public:
 
 	int dnn_width;
@@ -16,15 +14,16 @@ public:
 	float dnn_scale_div;
 	float threshold = 0.5f;
   float iou_threshold = 0.5;
-  int batchSize{ 1 };                  //!< Number of inputs in a batch
-	int dlaCore{ -1 };                   //!< Specify the DLA core to run network on.
-	bool int8{ false };                  //!< Allow runnning the network in Int8 mode.
-	bool fp16{ false };                  //!< Allow running the network in FP16 mode.
+        int batchSize{1};
+        int dlaCore{-1};
+        bool int8{false};
+        bool fp16{false};                
   bool is_use_mean_sub {true};
   bool is_nchw;
 	string modelFileName;
   bool anomalyEnable = false;;
-  vector<anomaly_var> vanomaly;
+  vector<int> order_of_feature_index_to_batch;
+  vector<anomaly_var> anomaly_feature;
 
   vector<double> mean{0.485, 0.456, 0.406};
   vector<double> std{0.229, 0.224, 0.225};
