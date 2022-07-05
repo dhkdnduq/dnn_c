0fb0c62a7616bd1574b3e50b29b4329e5de541d4
ojun <dhkdnduq@gmail.com>
2022-07-06 08:37:36 +0900

    Update dnn_impl.cpp

--- DL/dnn_impl.cpp
+++ DL/dnn_impl.cpp
@@ -27,17 +27,105 @@ bool dnn_impl::file_exists(const std::string fileName, bool verbose) {
   return true;
 }
 
-void dnn_impl::gen_dl_image(cv::Mat frame) {
-   ori_image_buffer.push_back(frame);
+void dnn_impl::gen_dl_image_cpu(cv::Mat frame) {
+  if (ori_image_buffer.size() > get_batch_size()) return;
+  ori_image_buffer.push_back(frame);
+  try {
+    cv::Mat gpu_frame = frame;
+    auto input_width = cfg_.dnn_width;
+    auto input_height = cfg_.dnn_height;
+    auto input_size = cv::Size(input_width, input_height);
+    //cv::imwrite("cpu_gpu_frame.jpg", gpu_frame);
+    if (cfg_.dnn_chnnel == 3) {
+      if (cfg_.letterboxing) {
+        int stride = 32;
+        float ratio = std::min(input_width / (float)frame.cols,
+                               input_height / (float)frame.rows);
+        auto new_unpad_w = int(std::round(frame.cols * ratio));
+        auto new_unpad_h = int(std::round(frame.rows * ratio));
+        auto dw = input_width - new_unpad_w;
+        auto dh = input_height - new_unpad_h;
+        dw = dw % stride;
+        dh = dh % stride;
+        dw /= 2.;
+        dh /= 2.;
+        cv::resize(gpu_frame, gpu_frame, {new_unpad_w, new_unpad_h});
+        int top = int(std::round(dh - 0.1));
+        int bottom = int(std::round(dh + 0.1));
+        int left = int(std::round(dw - 0.1));
+        int right = int(std::round(dw + 0.1));
+        cv::copyMakeBorder(gpu_frame, gpu_frame, top, bottom, left, right,
+                                 cv::BORDER_CONSTANT);
+      }
 
+      cv::Mat resized;
+      cv::resize(gpu_frame, resized, input_size, 0, 0);
+      cv::Mat flt_image;
+      //cv::imwrite("cpu_resized.jpg", resized);
+      if (frame.channels() == 1)
+        cv::cvtColor(resized, resized, cv::COLOR_GRAY2BGR);
+      //cv::imwrite("cpu_cvt.jpg", resized);
+      // if (cfg_.dnn_scale_div != 1)
+      resized.convertTo(flt_image, CV_32FC3, 1.f / cfg_.dnn_scale_div);
+      int channel = flt_image.channels();
+      // subtract mean
+      vector<cv::Mat> img_channels;
+      if (cfg_.is_use_mean_sub) {
+        cv::Mat subtract, divide;
+        cv::subtract(flt_image,
+                           cv::Scalar(cfg_.mean[2], cfg_.mean[1], cfg_.mean[0]),
+                           subtract);
+
+        cv::split(subtract, img_channels);
+        cv::divide(img_channels[0], cfg_.std[2], img_channels[0]);
+        cv::divide(img_channels[1], cfg_.std[1], img_channels[1]);
+        cv::divide(img_channels[2], cfg_.std[0], img_channels[2]);
+
+        cv::merge(img_channels, flt_image);
+        // cv::cuda::divide(subtract, cv::Scalar(cfg_.std[2], cfg_.std[1],
+        // cfg_.std[0]), flt_image);
+      }
+      //cv::imwrite("cpu_flt_image.jpg",flt_image);
+      preprocess_image_buffer.push_back(flt_image); 
+    }
+  } catch (...) {
+    ::cout << "failed preprocessImage\n";
+  }
+}
+
+
+void dnn_impl::gen_dl_image_gpu(cv::Mat frame) {
+  if (ori_image_buffer.size() > get_batch_size() )
+    return;
+   ori_image_buffer.push_back(frame);
    try {
      cv::cuda::GpuMat gpu_frame;
      gpu_frame.upload(frame);
-     auto input_width = cfg_.dnn_width;
+     auto input_width  = cfg_.dnn_width;
      auto input_height = cfg_.dnn_height;
      auto input_size = cv::Size(input_width, input_height);
 
      if (cfg_.dnn_chnnel == 3) {
+       if(cfg_.letterboxing)
+       {
+         int stride = 32;
+         float ratio = std::min(input_width / (float)frame.cols,input_height / (float)frame.rows);
+         auto new_unpad_w = int(std::round(frame.cols * ratio));
+         auto new_unpad_h = int(std::round(frame.rows * ratio));
+         auto dw = input_width - new_unpad_w;
+         auto dh = input_height - new_unpad_h;
+         dw = dw % stride;
+         dh = dh % stride;
+         dw /= 2.;
+         dh /= 2.;
+         cv::cuda::resize(gpu_frame, gpu_frame, {new_unpad_w, new_unpad_h});
+         int top = int(std::round(dh - 0.1));
+         int bottom = int(std::round(dh + 0.1));
+         int left = int(std::round(dw - 0.1));
+         int right = int(std::round(dw + 0.1));
+         cv::cuda::copyMakeBorder(gpu_frame,gpu_frame,top,bottom,left,right,cv::BORDER_CONSTANT);
+       }
+
        cv::cuda::GpuMat resized;
        cv::cuda::resize(gpu_frame, resized, input_size, 0, 0);
        cv::cuda::GpuMat flt_image;
@@ -54,19 +142,17 @@ void dnn_impl::gen_dl_image(cv::Mat frame) {
          cv::cuda::subtract(
              flt_image, cv::Scalar(cfg_.mean[2], cfg_.mean[1], cfg_.mean[0]),
              subtract);
+         
          cv::cuda::split(subtract, img_channels);
          cv::cuda::divide(img_channels[0], cfg_.std[2], img_channels[0]);
          cv::cuda::divide(img_channels[1], cfg_.std[1], img_channels[1]);
          cv::cuda::divide(img_channels[2], cfg_.std[0], img_channels[2]);
 
          cv::cuda::merge(img_channels, flt_image);
+         //cv::cuda::divide(subtract, cv::Scalar(cfg_.std[2], cfg_.std[1], cfg_.std[0]), flt_image);
        }
-
        //must be converted to mat, because of gpumat stride too long
-     
-       preprocess_image_buffer.push_back(cv::Mat(flt_image));
-    
-
+       preprocess_image_buffer.push_back(cv::Mat(flt_image)); 
      }
    } catch (...) {
      ::cout << "failed preprocessImage\n";
@@ -78,7 +164,10 @@ bool dnn_impl::add_image(const char* filepath)
   if (file_exists(filepath,true)) {
     Mat mat = imread(filepath, ImreadModes::IMREAD_COLOR);
     if (mat.empty()) return false;
-    gen_dl_image(mat);
+    if(cfg_.use_gpu_image_process)
+      gen_dl_image_gpu(mat);
+    else
+      gen_dl_image_cpu(mat);
   } else {
     return false;
   }
@@ -90,7 +179,12 @@ bool dnn_impl::add_image(const cv::Mat mat)
     cout << "buffer is empty";
     return false;
   }
-  gen_dl_image(mat);
+  if (cfg_.use_gpu_image_process)
+    gen_dl_image_gpu(mat);
+  else
+    gen_dl_image_cpu(mat);
+  
+
   return true;
 }
 bool dnn_impl::add_image(unsigned char* data, const size_t data_length)
@@ -101,10 +195,26 @@ bool dnn_impl::add_image(unsigned char* data, const size_t data_length)
     cout << "buffer is empty";
     return false;
   }
-  gen_dl_image(mat);
+  if (cfg_.use_gpu_image_process)
+    gen_dl_image_gpu(mat);
+  else
+    gen_dl_image_cpu(mat);
   return true;
 }
 
+void dnn_impl::fill_dummy_image_if_not_enough() {
+  int batch_size = get_batch_size();
+  if (ori_image_buffer.size() != batch_size) {
+    if (ori_image_buffer.size() > 1) {
+      for (int i = ori_image_buffer.size(); i < batch_size; i++)
+        add_image(Mat(ori_image_buffer[0].rows, ori_image_buffer[0].cols,
+                      ori_image_buffer[0].type()));
+    } else {
+      for (int i = ori_image_buffer.size(); i < batch_size; i++)
+        add_image(Mat(cfg_.dnn_height, cfg_.dnn_width, CV_8UC3));
+    }
+  }
+}
 
 cv::Mat dnn_impl::imageinfoToMat(image_info& imInfo) { 
   std::vector<char> data(imInfo.data, imInfo.data + imInfo.size);
@@ -116,7 +226,15 @@ void dnn_impl::matToImageinfo(cv::Mat src, image_info& imInfo) {
 
   vector<unsigned char> buf;
   imencode(".bmp", src, buf);
-  imInfo.gen(buf);
+
+  auto gen = [&](vector<unsigned char>& buf) {
+    imInfo.clear();
+    auto size = (int)buf.size();
+    auto data = (unsigned char*)calloc(size, sizeof(unsigned char));
+    std::copy(buf.begin(), buf.end(), data);
+  };
+
+  gen(buf);
  
  }
 
